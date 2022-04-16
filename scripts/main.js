d27f9fddf2818223098cf1636efcab0b1fe937f8
x0Byte <cybersecuritystudent@protonmail.com>
2022-04-16 20:31:05 +0100

    complete

--- scripts/main.js
+++ scripts/main.js
@@ -1,1 +1,61 @@
-console.log("Hello Player");
+game();
+
+function computerSelection() {
+  //randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+  let moves = ["rock", "paper", "scissors"];
+  let random = Math.floor(Math.random() * moves.length);
+  let choice = moves[random];
+
+  return choice;
+}
+
+function playerSelection() {
+  let choice = prompt("select your move [rock, paper or scissors]")
+    .trim()
+    .toLowerCase();
+
+  return choice;
+}
+
+function playRound(playerSelection, computerSelection) {
+  if (playerSelection === computerSelection) {
+    return "draw";
+  } else if (playerSelection === "rock" && computerSelection === "paper") {
+    return "computer";
+  } else if (playerSelection === "rock" && computerSelection === "scissors") {
+    return `player`;
+  } else if (playerSelection === "paper" && computerSelection === "scissors") {
+    return `computer`;
+  } else if (playerSelection === "paper" && computerSelection === "rock") {
+    return `player`;
+  } else if (playerSelection === "scissors" && computerSelection === "rock") {
+    return `computer`;
+  } else if (playerSelection === "scissors" && computerSelection === "paper") {
+    return `player`;
+  }
+}
+
+function game() {
+  let playerScore = 0;
+  let computerScore = 0;
+
+  for (let i = 0; i < 5; i++) {
+    let round = playRound(playerSelection(), computerSelection());
+
+    if (round === "player") {
+      playerScore++;
+      console.log(`Round goes to player`);
+    } else if (round === "computer") {
+      computerScore++;
+      console.log(`Round goes to computer`);
+    } else if (round === "draw") {
+      console.log(`Draw`);
+    }
+  }
+
+  if (playerScore > computerScore) {
+    console.log(`You Win!! ${playerScore} - ${computerScore}`);
+  } else {
+    console.log(`You Loose!! ${computerScore} - ${playerScore}`);
+  }
+}
